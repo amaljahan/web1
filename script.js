2428485a51424c3cf27e5e943cc836f42321d30f
amaljahan <amaljahan3@gmail.com>
2024-06-20 13:42:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function settingsMenuToggle() {
+    
+    // var settingsmenu= document.querySelector(".settings-menu");
+    // console.log("hgfuyfuyg",settingsmenu);
+    
+    // let setMENU=document.getElementById("settings-menu")
+    // setMENU.classList.remove("Settings-menu"); 
+    // setMENU.classList.add("settings-menu-height")
+    
+      //the toggle used, its function is if the settings-menu-height is existing remove it if it not existing create it.
+}
+
+// profile icon press cheyyumbol settingsum logoutum mattum reveal aavanam athaan ini
+// cheyyanullath
+   
